c80fb07f2c26a90ba9cf8aa33c482834ba43b33c
sebastian-vivas <seb.vivas.dev@gmail.com>
2020-02-26 18:39:57 -0500

    my changes

---
+++ expressions_matter.js
@@ -0,0 +1,6 @@
+/*Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ().*/
+
+
+function expressionMatter(a, b, c) {
+  return Math.max(a + b + c, a * (b + c), (a + b) * c, a * b * c);
+}
