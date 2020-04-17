3622220e39fd7817f5c06164b93f55ca925cc600
Sebastian Vivas <60440088+sebastian-vivas@users.noreply.github.com>
2020-04-17 10:24:08 -0400

    Update how_good_are_you.js

--- how_good_are_you.js
+++ how_good_are_you.js
@@ -3,6 +3,8 @@ But you're an ambitious person. You want to know if you're better than the avera
 
 You receive an array with your peers' test scores. Now calculate the average and compare your score!
 
+
+
 Return True if you're better, else False!*/
 
 function betterThanAverage(classPoints, yourPoints) {
