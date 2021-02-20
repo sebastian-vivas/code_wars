38fde3ed7a0f7c25c2bdf657c61bb2cb75faed80
Sebastian Vivas <seb.vivas.dev@gmail.com>
2021-02-20 17:05:22 -0500

    7kyu Codewar

---
+++ switcheroo.js
@@ -0,0 +1,13 @@
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+// Example:
+
+// 'acb' --> 'bca'
+// 'aabacbaa' --> 'bbabcabb'
+
+function switcheroo(x){
+    const chars = {'a':'b','b':'a'};
+    let str = x;
+    str = str.replace(/[ab]/g, m => chars[m]);
+    return str
+  };
